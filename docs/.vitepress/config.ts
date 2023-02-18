011586ab2fac1b2fdd4007801169f0c7845cc092
yangjin <yangjin.im@bytedance.com>
2023-02-18 16:01:53 +0800

    feat: init vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,82 @@
+export default {
+  title: 'typescript tutorial',
+  description: 'Just Start TypeScript.',
+  themeConfig: {
+    nav: [
+      { text: 'fe tutorial', link: 'https://niexia.github.io/fe-tutorial/javascript/programming-language/' },
+      { text: 'blog', link: 'https://niexia.github.io' }
+    ],
+    sidebar: [
+      {
+        text: 'Getting Start',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      },
+      {
+        text: 'Basic Types',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      },
+      {
+        text: 'Control Flow Statements',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      },
+      {
+        text: 'Functions',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      },
+      {
+        text: 'Interfaces',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      },
+      {
+        text: 'Advanced Types',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      },
+      {
+        text: 'Generics',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      },
+      {
+        text: 'Modules',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      },
+      {
+        text: 'Type in Vue.js',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      },
+      {
+        text: 'Challenge',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ]
+      }
+    ]
+  }
+}
